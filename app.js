3d297ed57ec151af4aded82707e48fe8ee12bb12
aboli3514 <39972127+aboli3514@users.noreply.github.com>
2021-11-11 09:46:28 -0500

    Update app.js
    1 step closer to multi-server support

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ bot.login(auth.DISCORD_TOKEN);
 bot.on('ready', () => {
     console.log(getTime() + ' bot has started');
     checkForNewGuilds();
-    setInterval(checkForNewGuilds,30000);
+    setInterval(checkForNewGuilds,5000);
 });
 
 bot.on('message', async message => {
@@ -26,115 +26,133 @@ function pingForWar() {
 }
 
 function updateMemberRoles() {
-    //TODO --> Put this entire block inside of a loop that goes through each 
-
-    client.clanByTag(clanTag).then(response => {
-        let clanMembers = response.memberList;
-        let guildMembers = guild.members.cache;
-        guildMembers.each(member => {
-            if (member.roles.cache.array().length == 1) {
-                member.roles.add(newMemberRole);
-            }
-            if (member.roles.cache.find(r => r.name === newMemberRole.name) != newMemberRole ^ member.roles.cache.find(r => r.name === botsRole.name) == botsRole) {
-                if (member.roles.cache.find(r => r.name === leaderRole.name) === leaderRole) {
-                    for (i = 0; i < clanMembers.length; i++) {
-                        if (clanMembers[i].name == member.nickname) {
-                            if (clanMembers[i].role != 'leader') {
-                                console.log(getTime() + ' leader ' + member.nickname + ' ' + member.roles.cache.array()[0].name + ' ' + clanMembers[i].role);
-                                member.roles.remove(leaderRole);
-                                if (clanMembers[i].role == 'coLeader') {
-                                    member.roles.add(coleaderRole);
-                                } else if (clanMembers[i].role == 'admin') {
-                                    member.roles.add(elderRole);
-                                } else {
-                                    member.roles.add(memberRole);
-                                }
-                            }
-                        }
-                    }
-                } else if (member.roles.cache.find(r => r.name === coleaderRole.name) === coleaderRole) {
-                    for (i = 0; i < clanMembers.length; i++) {
-                        if (clanMembers[i].name == member.nickname) {
-                            if (clanMembers[i].role != 'coLeader') {
-                                console.log(getTime() + ' coleader ' + member.nickname + ' ' + member.roles.cache.array()[0].name + ' ' + clanMembers[i].role);
-                                member.roles.remove(coleaderRole);
-                                if (clanMembers[i].role == 'leader') {
-                                    member.roles.add(leaderRole);
-                                } else if (clanMembers[i].role == 'admin') {
-                                    member.roles.add(elderRole);
-                                } else {
-                                    member.roles.add(memberRole);
-                                }
-                            }
-                        }
-                    }
-                } else if (member.roles.cache.find(r => r.name === elderRole.name) === elderRole) {
-                    for (i = 0; i < clanMembers.length; i++) {
-                        if (clanMembers[i].name == member.nickname) {
-                            if (clanMembers[i].role != 'admin') {
-                                console.log(getTime() + ' elder ' + member.nickname + ' ' + member.roles.cache.array()[0].name + ' ' + clanMembers[i].role);
-                                member.roles.remove(elderRole);
-                                if (clanMembers[i].role == 'coLeader') {
-                                    member.roles.add(coleaderRole);
-                                } else if (clanMembers[i].role == 'leader') {
-                                    member.roles.add(leaderRole);
-                                } else {
-                                    member.roles.add(memberRole);
-                                }
-                            }
-                        }
-                    }
-                } else if (member.roles.cache.find(r => r.name === memberRole.name) === memberRole) {
-                    for (i = 0; i < clanMembers.length; i++) {
-                        if (clanMembers[i].name == member.nickname) {
-                            if (clanMembers[i].role != 'member') {
-                                console.log(getTime() + ' member ' + member.nickname + ' ' + member.roles.cache.array()[0].name + ' ' + clanMembers[i].role);
-                                member.roles.remove(memberRole);
-                                if (clanMembers[i].role == 'coLeader') {
-                                    member.roles.add(coleaderRole);
-                                } else if (clanMembers[i].role == 'admin') {
-                                    member.roles.add(elderRole);
-                                } else {
-                                    member.roles.add(leaderRole);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+    guilds.each(guildID => {
+        let clanTag;
+        let guild = bot.guilds.cache.find(g => g.id === guildID);
+        let initialized;
+        fs.readFile(guildID + '/settings.json', (err, data) => {
+            initialized = data.initialized;
+            clanTag = data.clanTag;
         });
-    }).catch(err => {
-        console.log(getTime() + ' ' + err);
+        if (initialized == true) {
+            client.clanByTag(clanTag).then(response => {
+                let clanMembers = response.memberList;
+                let guildMembers = guild.members.cache;
+                guildMembers.each(member => {
+                    if (member.roles.cache.array().length == 1) {
+                        member.roles.add(newMemberRole);
+                    }
+                    if (member.roles.cache.find(r => r.name === newMemberRole.name) != newMemberRole 
+                        ^ member.roles.cache.find(r => r.name === botsRole.name) == botsRole) {
+                        if (member.roles.cache.find(r => r.name === leaderRole.name) === leaderRole) {
+                            for (i = 0; i < clanMembers.length; i++) {
+                                if (clanMembers[i].name == member.nickname) {
+                                    if (clanMembers[i].role != 'leader') {
+                                        console.log(getTime() + ' leader ' + member.nickname + ' ' 
+                                        + member.roles.cache.array()[0].name + ' ' + clanMembers[i].role);
+                                        member.roles.remove(leaderRole);
+                                        if (clanMembers[i].role == 'coLeader') {
+                                            member.roles.add(coleaderRole);
+                                        } else if (clanMembers[i].role == 'admin') {
+                                            member.roles.add(elderRole);
+                                        } else {
+                                            member.roles.add(memberRole);
+                                        }
+                                    }
+                                }
+                            }
+                        } else if (member.roles.cache.find(r => r.name === coleaderRole.name) === coleaderRole) {
+                            for (i = 0; i < clanMembers.length; i++) {
+                                if (clanMembers[i].name == member.nickname) {
+                                    if (clanMembers[i].role != 'coLeader') {
+                                        console.log(getTime() + ' coleader ' + member.nickname + ' ' 
+                                            + member.roles.cache.array()[0].name + ' ' + clanMembers[i].role);
+                                        member.roles.remove(coleaderRole);
+                                        if (clanMembers[i].role == 'leader') {
+                                            member.roles.add(leaderRole);
+                                        } else if (clanMembers[i].role == 'admin') {
+                                            member.roles.add(elderRole);
+                                        } else {
+                                            member.roles.add(memberRole);
+                                        }
+                                    }
+                                }
+                            }
+                        } else if (member.roles.cache.find(r => r.name === elderRole.name) === elderRole) {
+                            for (i = 0; i < clanMembers.length; i++) {
+                                if (clanMembers[i].name == member.nickname) {
+                                    if (clanMembers[i].role != 'admin') {
+                                        console.log(getTime() + ' elder ' + member.nickname + ' ' 
+                                            + member.roles.cache.array()[0].name + ' ' + clanMembers[i].role);
+                                        member.roles.remove(elderRole);
+                                        if (clanMembers[i].role == 'coLeader') {
+                                            member.roles.add(coleaderRole);
+                                        } else if (clanMembers[i].role == 'leader') {
+                                            member.roles.add(leaderRole);
+                                        } else {
+                                            member.roles.add(memberRole);
+                                        }
+                                    }
+                                }
+                            }
+                        } else if (member.roles.cache.find(r => r.name === memberRole.name) === memberRole) {
+                            for (i = 0; i < clanMembers.length; i++) {
+                                if (clanMembers[i].name == member.nickname) {
+                                    if (clanMembers[i].role != 'member') {
+                                        console.log(getTime() + ' member ' + member.nickname + ' ' 
+                                            + member.roles.cache.array()[0].name + ' ' + clanMembers[i].role);
+                                        member.roles.remove(memberRole);
+                                        if (clanMembers[i].role == 'coLeader') {
+                                            member.roles.add(coleaderRole);
+                                        } else if (clanMembers[i].role == 'admin') {
+                                            member.roles.add(elderRole);
+                                        } else {
+                                            member.roles.add(leaderRole);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            }).catch(err => {
+                console.log(getTime() + ' ' + err);
+            });
+        }
     });
+}
+
+
+function newGuild(guild) {
+    let guilds = info.guilds;
+    guilds.push(guild.id);
+    fs.mkdir(guild.id, err => {
+        if (err) {
+            console.log(getTime() + ' ' + err);
+        }
+    });
+    fs.open(guild.id + '/settings.json','w+', err => {
+        if (err) {
+            console.log(getTime + ' ' + err);
+        }
+    });
+    let initialized = {
+        initialized : false
+    }
+    fs.writeFileSync(guild.id + '/settings.json', JSON.stringify(initialized));
+    let guildInfo = {
+        guilds : guilds
+    }
+    let guildInfoJSON = JSON.stringify(guildInfo);
+    fs.writeFileSync('guildInfo.json', guildInfoJSON);
+    info = JSON.parse(fs.readFileSync('guildInfo.json'));
 }
 
 function checkForNewGuilds() {
     if (info.guilds == undefined) {
-        let guilds = [];
         bot.guilds.cache.each(guild => {
-            guilds.push(guild.id);
-            fs.mkdir(guild.id, err => {
-                if (err) {
-                    console.log(getTime() + ' ' + err);
-                }
-            });
-            fs.open(guild.id + '/settings.json','w+', err => {
-                if (err) {
-                    console.log(getTime + ' ' + err);
-                }
-            });
-            let initialized = {
-                initialized : false
-            }
-            fs.writeFileSync(guild.id + '/settings.json', JSON.stringify(initialized));
+            newGuild(guild);
         });
-        let guildInfo = {
-            guilds : guilds
-        }
-        let guildInfoJSON = JSON.stringify(guildInfo);
-        fs.writeFileSync('guildInfo.json', guildInfoJSON);
-        info = JSON.parse(fs.readFileSync('guildInfo.json'));
-        
     } else {
         let guilds = [];
         for (let i = 0; i < info.guilds.length; i++) {
@@ -145,7 +163,7 @@ function checkForNewGuilds() {
                 }
             })
             if (!found) {
-                fs.rmdir(`${info.guilds[i]}`, err => {
+                fs.rmdir(`${info.guilds[i]}`, {recursive: true}, err => {
                     if (err) {
                         console.log(getTime() + ' ' + err);
                     }
@@ -162,26 +180,9 @@ function checkForNewGuilds() {
                 }
             }
             if (!found) {
-                guilds.push(guild.id);
-                fs.mkdir(guild.id, err => {
-                    if (err) {
-                        console.log(getTime() + ' ' + err);
-                    }
-                });
-                fs.open(guild.id + '/settings.json','w+', er => {
-                    if (err) {
-                        console.log(getTime + ' ' + err);
-                    }
-                });
-                fs.writeFileSync(guild.id + '/settings.json', JSON.stringify(initialized));
+                newGuild(guild);
             }
         });
-        let guildInfo = {
-            guilds : guilds
-        }
-        let guildInfoJSON = JSON.stringify(guildInfo);
-        fs.writeFileSync('guildInfo.json', guildInfoJSON);
-        info = JSON.parse(fs.readFileSync('./guildInfo.json'));
     }
 }
 
@@ -190,14 +191,8 @@ function getTime() {
     return (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
 }
 
-function deleteInvalidTagMessage() {
-    verificationChannel.lastMessage.delete();
-}
 
-function deleteVerMessage() {
-    verificationChannel.lastMessage.delete().catch(err => {});
-}
 
-function deleteBotMessage() {
-    botCommandsChannel.lastMessage.delete().catch(err => {});
+function deleteMessage(channel) {
+    channel.lastMessage.delete().catch(err => {});
 }
